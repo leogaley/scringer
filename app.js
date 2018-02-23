b1f22813863b8b7670267cea770abe81c2eb3885
Leo Galey <leogaley@gmail.com>
2018-02-23 14:36:30 -0600

    update file names

--- app.js
+++ app.js
@@ -32,9 +32,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scringer";
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI,{
-  useMongoClient:true
-});
+mongoose.connect(MONGODB_URI);
 
 
 app.get("/delete",function(req,res){
